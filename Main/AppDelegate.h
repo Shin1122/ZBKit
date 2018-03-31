bf2e776604cd66f3a0900fa02b935c2eb2cbab1c
Shin1122 <big_shin@126.com>
2018-03-31 20:56:48 +0800

    Initalize

---
+++ Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBKit
+//
+//  Created by Lakeside on 2018/3/31.
+//  Copyright © 2018年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZBKit
+//
+//  Created by Lakeside on 2018/3/31.
+//  Copyright © 2018年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
